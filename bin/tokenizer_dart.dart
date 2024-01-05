59d50e4266e85a17560a4e861136a8f99bcf8106
Jonas Gabriel <jonas.rusiana0@gmail.com>
2024-01-05 17:39:15 +0800

    Compile Exe

--- bin/tokenizer_dart.dart
+++ bin/tokenizer_dart.dart
@@ -1,16 +1,33 @@
 import 'package:args/args.dart';
 import 'dart:io';
 
+/// Tokenizer Dart
+/// 
+/// A tokenizer made in dart. It has two methods to tokenize, one is the extraction process based on a delimiter.
+/// The other is the identification which conducts lexical analysis on what type of token it is. This was 
+/// entirely made using Dart with little to no tokenizer library, even a split string method. This was all
+/// possible with the use of the token data structure and regular expressions.
+/// 
+/// Author: Jonas and Gwyn
+
 // Note: This test case: "Hello! World! Hello World! Hello World !\nTest!,!" has two possible outcomes.
 // One outcome is for the entered string in the dart file and the text file, while the other is the CLI outcome.
 // The first outcome recognizes \nTest as separate, while the other outcome recognizes the other as a one.
 // This is due to the fact that the terminal in general has different code for a new line and not applicable directly to
 // the program.
 
+/// TokenType Enumeration
+/// 
+/// This makes sure that tokens are properly categorized. This also ensures a proper
+/// development quality.
 enum TokenType {
   word, punctuation, phrase, number, empty, eol, eof, unkown,
 }
 
+/// Token Data Structure
+/// 
+/// This contains the necessary data types on what makes a token a token. Also contains a 
+/// simple toString() method when printed as a string.
 class Token {
   String value;
   TokenType type;
@@ -30,8 +47,16 @@ class Token {
   }
 }
 
+/// Tokens List Variable
+/// 
+/// This is a global variable that contains all processed and identified tokens.
+/// Essesntially the collection of all tokens that was created from a string.
 List<Token> tokens = [];
 
+/// The Main Method
+/// 
+/// Uses an args parser for a CLI frontend. This also outputs
+/// the tokens that was processed and identified.
 void main(List<String> arguments) {
   ArgParser parser = ArgParser();
   
@@ -41,11 +66,6 @@ void main(List<String> arguments) {
   parser.addOption('filename', abbr: 'n', defaultsTo: '');
  
   ArgResults results = parser.parse(arguments);
-
-  // String testString = "Hello! World! Hello World! Hello World !\nTest!,!";
-  // tokens = identify(
-  //   extract(testString)
-  // );
 
   String string = results['string'];
   bool preprocess = bool.parse(results['preprocess']);
@@ -72,6 +92,13 @@ void main(List<String> arguments) {
   }
 }
 
+/// Extract
+/// 
+/// This extracts the tokens from the string through a for loop method. It loops every character
+/// checking for a delimiter or an end line. And collects it in the currentToken variable.
+/// Once a delimiter is found, the currentToken is then recorded, trimmed, and create a new token upon
+/// its value. Then the new token is put on the list to be then outputted. Note that the tokens are 
+/// not yet identified.
 List<Token> extract(String string, {bool preprocess = true, bool usingFile = false}) {
   List<Token> rawTokens = [];
   String currentToken = "";
@@ -131,6 +158,13 @@ List<Token> extract(String string, {bool preprocess = true, bool usingFile = fal
   return rawTokens;
 }
 
+/// Identify
+/// 
+/// This loops through the entire token list that was provided as an argument.
+/// And uses other methods to check whether or not they're a certain token or not.
+/// If they are, they are classified as is. The loop is a bit faster now considering 
+/// it only loops the tokens, and not every character. Making it an efficient way of 
+/// identification.
 List<Token> identify(List<Token> tokens) {
   for (Token token in tokens) {
     if (token.type == TokenType.eol || token.type == TokenType.eof) continue;
@@ -148,6 +182,9 @@ List<Token> identify(List<Token> tokens) {
   return tokens;
 }
 
+/// changeType
+/// 
+/// This is a utility method that enables a one liner statement for the identification method.
 bool changeType(bool condition, Token token, TokenType type) {
   if (condition) {
     token.type = type;
@@ -156,27 +193,42 @@ bool changeType(bool condition, Token token, TokenType type) {
   return false;
 }
 
+/// isNumeric
+/// 
+/// Checks whether the string provided is a number or not based on the given regex.
 bool isNumeric(String s) {
   final regex = RegExp(r'^-?(([0-9]*)|(([0-9]*)\.([0-9]*)))$');
   return regex.hasMatch(s);
 }
 
+/// isAlphabetical
+/// 
+/// Checks whether the string provided is a word or word with punctuation based on the given regex.
 bool isAlphabetical(String s) {
   final wordAlone = RegExp(r'^[a-zA-Z]+$');
   final withPunctuation = RegExp(r"(?<=\s|^|\b)(?:[-'.%$#&\/]\b|\b[-'.%$#&\/]|[A-Za-z0-9]|\([A-Za-z0-9]+\))+(?=\s|$|\b)");
   return wordAlone.hasMatch(s) || withPunctuation.hasMatch(s);
 }
 
+/// isPhrase
+/// 
+/// Checks whether the string is a phrase or not if it contains a lot of spaces or not.
 bool isPhrase(String s) {
   final regex = RegExp(r"\s\b|\b\s");
   return regex.hasMatch(s);
 }
 
+/// isPunctuation
+/// 
+/// Checks whether it the string is a punctation or not.
 bool isPunctuation(String s) {
   final regex = RegExp(r'[^\w\s]+');
   return regex.hasMatch(s);
 }
 
+/// isUnknown
+/// 
+/// If all else fails, it will be identified as unknown.
 bool isUnknown(String s) {
   return !(isNumeric(s) && isAlphabetical(s) && isPhrase(s) && isPunctuation(s));
 }
